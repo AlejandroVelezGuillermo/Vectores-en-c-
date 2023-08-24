a10abbbd2bd98fcad0933bed4e12bebb2678f4e0
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-08-24 16:20:40 -0600

    Add files via upload

---
+++ Vectores/Vectores/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Vectores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lstVectores = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(55, 102);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(115, 158);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "GENERAL VECTOR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lstVectores
+            // 
+            this.lstVectores.FormattingEnabled = true;
+            this.lstVectores.ItemHeight = 16;
+            this.lstVectores.Location = new System.Drawing.Point(218, 18);
+            this.lstVectores.Name = "lstVectores";
+            this.lstVectores.Size = new System.Drawing.Size(518, 420);
+            this.lstVectores.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstVectores);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ListBox lstVectores;
+    }
+}
+
---
+++ Vectores/Vectores/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            // Generar un vector aleatorio de longitud 5
+            Random random = new Random();
+            double[] vector = new double[5];
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = random.NextDouble() * 10; // Números aleatorios entre 0 y 10
+            }
+
+            // Convertir el vector a una cadena para mostrarlo en la ListBox
+            string vectorString = "[" + string.Join(", ", vector) + "]";
+
+            // Agregar el vector a la ListBox
+            lstVectores.Items.Add(vectorString);
+        }
+    }
+}
